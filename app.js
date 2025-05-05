29a3c4d0b87012fc58a207dee551983e3ea8cc0b
Aoua <aoua.sow1@gmail.com>
2025-05-05 13:10:41 -0400

    update

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const jwt = require('jwt-simple')
 const User = require('./models/user')
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://aouasow1.github.io'
+}));
+
 app.use(express.json());
 const router = express.Router()
 const secret = 'supersecret'
